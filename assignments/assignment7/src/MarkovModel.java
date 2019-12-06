de4cf3ce7585c4e4c05f7668c8dc8ca4e5e50cf6
Sarah <sarahpham99@hotmail.com>
2019-12-06 11:32:08 -0600

    Assignment 7 done

--- assignments/assignment7/src/MarkovModel.java
+++ assignments/assignment7/src/MarkovModel.java
@@ -1,3 +1,147 @@
+//import java.io.File;
+//import java.util.HashMap;
+//import java.io.IOException;
+//import java.util.Random;
+//import java.util.Scanner;
+//import java.util.Set;
+//
+///**
+// * MarkovModel.java Creates an order K Markov model of the supplied source
+// * text. The value of K determines the size of the "kgrams" used to generate
+// * the model. A kgram is a sequence of k consecutive characters in the source
+// * text.
+// *
+// * @author     Sarah Pham (slp0042@auburn.edu)
+// * @author     Dean Hendrix (dh@auburn.edu)
+// * @version    2018-04-17
+// *
+// */
+//public class MarkovModel {
+//
+//   // Map of <kgram, chars following> pairs that stores the Markov model.
+//   private HashMap<String, String> model;
+//
+//   // add other fields as you need them ...
+//
+//   private String sourceText = "";
+//   private int K;
+//   private int length;
+//
+//   /**
+//    * Reads the contents of the file sourceText into a string, then calls
+//    * buildModel to construct the order K model.
+//    *
+//    * DO NOT CHANGE THIS CONSTRUCTOR.
+//    *
+//    */
+//   public MarkovModel(int K, File sourceText) {
+//      model = new HashMap<>();
+//      try {
+//         String text = new Scanner(sourceText).useDelimiter("\\Z").next();
+//         buildModel(K, text);
+//      }
+//      catch (IOException e) {
+//         System.out.println("Error loading source text: " + e);
+//      }
+//   }
+//
+//
+//   /**
+//    * Calls buildModel to construct the order K model of the string sourceText.
+//    *
+//    * DO NOT CHANGE THIS CONSTRUCTOR.
+//    *
+//    */
+//   public MarkovModel(int K, String sourceText) {
+//      model = new HashMap<>();
+//      buildModel(K, sourceText);
+//   }
+//
+//
+//   /**
+//    * Builds an order K Markov model of the string sourceText.
+//    */
+//   private void buildModel(int K, String sourceText) {
+//	   this.sourceText = sourceText;
+//	   this.K = K;
+//	   this.length = sourceText.length();
+//
+//	   
+//	   for(int i = 0; i < length-K; i++){
+//		   String key = sourceText.substring(i, i + K);
+//		   
+//		   String value = model.getOrDefault(key, "").concat(sourceText.substring(i+K, i+K+1));
+//		   
+//		   model.put(key, value);
+//	   }
+//   }
+//
+//
+//   /** Returns the first kgram found in the source text. */
+//   public String getFirstKgram() {
+//	   return sourceText.substring(0, K);
+//   }
+//
+//
+//   /** Returns a kgram chosen at random from the source text. */
+//   public String getRandomKgram() {
+//      
+//	  Random rnd = new Random();
+//      int index = rnd.nextInt(this.length - K + 1);
+//      
+//	  return sourceText.substring(index, index + this.K);
+//   }
+//
+//
+//   /**
+//    * Returns the set of kgrams in the source text.
+//    *
+//    * DO NOT CHANGE THIS METHOD.
+//    *
+//    */
+//    public Set<String> getAllKgrams() {
+//      return model.keySet();
+//   }
+//
+//
+//   /**
+//    * Returns a single character that follows the given kgram in the source
+//    * text. This method selects the character according to the probability
+//    * distribution of all characters that follow the given kgram in the source
+//    * text.
+//    */
+//   public char getNextChar(String kgram) {
+//	   
+//	   Random rnd = new Random();
+//	   String value = model.get(kgram);
+//
+//	   while (value == null) {
+//		   value = model.get(this.getRandomKgram());
+//	   }
+//
+//	   int valueLen = value.length();
+//	   
+//	   char character = value.charAt(rnd.nextInt(valueLen));
+//	    	
+//	   return character;
+//	
+//   }
+//
+//
+//   /**
+//    * Returns a string representation of the model.
+//    * This is not part of the provided shell for the assignment.
+//    *
+//    * DO NOT CHANGE THIS METHOD.
+//    *
+//    */
+//    @Override
+//    public String toString() {
+//      return model.toString();
+//   }
+//
+//}
+
 import java.io.File;
 import java.util.HashMap;
 import java.io.IOException;
@@ -31,7 +175,7 @@ public class MarkovModel {
     * Reads the contents of the file sourceText into a string, then calls
     * buildModel to construct the order K model.
     *
-    * DO NOT CHANGE THIS CONSTRUCTOR.assignment 
+    * DO NOT CHANGE THIS CONSTRUCTOR.
     *
     */
    public MarkovModel(int K, File sourceText) {
@@ -87,7 +231,7 @@ public class MarkovModel {
    public String getRandomKgram() {
       
 	  Random rnd = new Random();
-      int index = rnd.nextInt(this.length - K);
+      int index = rnd.nextInt(this.length - K + 1);
       
 	  return sourceText.substring(index, index + this.K);
    }
@@ -114,10 +258,15 @@ public class MarkovModel {
 	   
 	   Random rnd = new Random();
 	   String value = model.get(kgram);
+       
+       while(value == null){
+           value = model.get(this.getRandomKgram());   
+       }
+	   
 	   int valueLen = value.length();
 	   
 	   return value.charAt(rnd.nextInt(valueLen));
-	   
+	
    }
 
 
--- assignments/assignment7/src/TextGenerator.java
+++ assignments/assignment7/src/TextGenerator.java
@@ -1,4 +1,84 @@
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//
+///**
+// * TextGenerator.java. Creates an order K Markov model of the supplied source
+// * text, and then outputs M characters generated according to the model.
+// *
+// * @author     Sarah Pham (slp0042@auburn.edu)
+// * @author     Dean Hendrix (dh@auburn.edu)
+// * @version    2018-04-17
+// *
+// */
+//public class TextGenerator {
+//
+//   /** Drives execution. */
+//   public static void main(String[] args) {
+//	   
+//	  /*
+//      if (args.length < 3) {
+//         System.out.println("Usage: java TextGenerator k length input");
+//         return;
+//      }
+//
+//      // No error checking! You may want some, but it's not necessary.
+//      int K = Integer.parseInt(args[0]);
+//      int M = Integer.parseInt(args[1]);
+//      if ((K < 0) || (M < 0)) {
+//         System.out.println("Error: Both K and M must be non-negative.");
+//         return;
+//      }
+//
+//      File text;
+//      try {
+//         text = new File(args[2]);
+//         if (!text.canRead()) {
+//            throw new Exception();
+//         }
+//         if(text.length() <= K) {
+//        	 System.out.println("error: source not enough characters");
+//        	throw new Exception();
+//         }
+//      }
+//      catch (Exception e) {
+//         System.out.println("Error: Could not open " + args[2] + ".");
+//         return;
+//      }
+//      */
+//      File input = new File("/home/sarah/Downloads/input_files/input_files/LM_input4.txt");
+//	   
+//	  //String input = "ABCDE\n";
+//      // instantiate a MarkovModel with the supplied parameters and
+//      // generate sample output text ...	
+//	  int K = 1;
+//	  int M = 1000;
+//      MarkovModel model = new MarkovModel(K, input);
+//      String kgram = model.getRandomKgram();
+//      String sampleText = kgram;
+//      
+//      //for(int i = 0; i < M; i++) {
+//      while(sampleText.length() < M) {
+//    	  char nextChar = model.getNextChar(kgram);
+//    	  sampleText = sampleText + nextChar;
+//    	  
+//    	  while (nextChar == 0) {
+//    		  kgram = model.getRandomKgram();
+//    		  sampleText = sampleText + kgram;
+//    		  nextChar = model.getNextChar(kgram);
+//    	  }
+//    	  
+//    	  kgram = sampleText.substring(sampleText.length() - K, sampleText.length());
+//      }
+//      
+//
+//	  System.out.println("Sample text: " + sampleText);
+//
+//   }
+//}
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
@@ -14,6 +94,7 @@ public class TextGenerator {
 
    /** Drives execution. */
    public static void main(String[] args) {
+	   
 
       if (args.length < 3) {
          System.out.println("Usage: java TextGenerator k length input");
@@ -34,6 +115,10 @@ public class TextGenerator {
          if (!text.canRead()) {
             throw new Exception();
          }
+         if(text.length() <= K) {
+        	 System.out.println("error: source not enough characters");
+        	throw new Exception();
+         }
       }
       catch (Exception e) {
          System.out.println("Error: Could not open " + args[2] + ".");
@@ -48,11 +133,28 @@ public class TextGenerator {
       String kgram = model.getRandomKgram();
       String sampleText = kgram;
       
+       /**
       for(int i = 0; i < M; i++) {
     	  sampleText = sampleText + model.getNextChar(kgram);
     	  kgram = sampleText.substring(sampleText.length() - K, sampleText.length());
-    	  System.out.println("Sample text: " + sampleText);
       }
+      **/
+       
+       while(sampleText.length()
+ < M){
+        char nextChar = model.getNextChar(kgram);
+           
+           while(nextChar == 0){
+             kgram = model.getRandomKgram();
+               sampleText = sampleText + kgram;
+               nextChar = model.getNextChar(kgram);
+           
+           }
+           
+           sampleText = sampleText + nextChar;
+           kgram = sampleText.substring(sampleText.length() - K, sampleText.length());
+       }
+      System.out.println(sampleText);
 
    }
 }
